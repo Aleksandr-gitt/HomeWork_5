3e99ae3b9b1dee116debd01bcc16d16b909a43d3
Aleksandr-Git <sanyasan123@mail.ru>
2023-07-09 19:51:56 +0300

    Task 2

--- TAsk_2.java
+++ TAsk_2.java
@@ -3,8 +3,13 @@
  */
 
 package HomeWork_5;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
 public class TAsk_2 {
     public static void main(String[] args) {
         String listmen = "Ваня, Петя, Миша, Саша, Ирина, Ольга, Даша, Петя, Ирина, Ольга, Ваня, Петя, Миша, Саша, Ирина, Ольга";
@@ -23,6 +28,16 @@ public class TAsk_2 {
         }
 
         System.out.println(countmen);
+
+        List <Map.Entry<String,Integer>> valuesList = new ArrayList(countmen.entrySet());
+        Collections.sort(valuesList, new Comparator<Map.Entry<String,Integer>>() {
+            public int compare(Map.Entry<String,Integer> o1, Map.Entry<String,Integer> o2) {
+                return o2.getValue().compareTo(o1.getValue()); // по убыванию
+                //return o1.getValue().compareTo(o2.getValue()); // по возрастанию
+            }
+        });
+
+        System.out.println(valuesList); 
     }
     
 }
